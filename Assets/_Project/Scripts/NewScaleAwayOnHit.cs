1e92fc56d1fdbcd59b734e17cc9198de7419bdea
ZhongTheKong <guoyizhong2004@gmail.com>
2026-04-18 22:29:21 -0400

    Work on trying to fix 9 section sword

--- Assets/_Project/Scripts/NewScaleAwayOnHit.cs
+++ Assets/_Project/Scripts/NewScaleAwayOnHit.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
@@ -39,21 +40,46 @@ public class NewScaleAwayOnHit : MonoBehaviour
     float volume;
     float maxVolumeShift;
     private float lastHitTime = 0f;
+    List<GameObject> leftComponents;
+    List<GameObject> middleXComponents;
+    List<GameObject> rightComponents;
+    List<GameObject> topComponents;
+    List<GameObject> middleYComponents;
+    List<GameObject> bottomComponents;
+    List<GameObject> allComponents;
+    Vector3 leftEdgeSize;
+    Vector3 rightEdgeSize;
+    Vector3 topEdgeSize;
+    Vector3 bottomEdgeSize;
 
     void Start()
     {
+        leftComponents = new() {top_left_corner, left_edge, bottom_left_corner};
+        middleXComponents = new() {left_edge, right_edge};
+        rightComponents = new() {top_right_corner, left_edge, bottom_right_corner};
+        topComponents = new() {top_left_corner, top_edge, top_right_corner};
+        middleYComponents = new() {top_edge, bottom_edge};
+        bottomComponents = new() {bottom_left_corner, bottom_edge, bottom_right_corner};
+        allComponents = leftComponents.Concat(middleXComponents).Concat(rightComponents).ToList();
+
+        leftEdgeSize = left_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+        rightEdgeSize = left_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+        topEdgeSize = left_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+        bottomEdgeSize = left_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+
         maxVolumeShift = 0.002f * volumeShiftModifier; // Upper limit
         Debug.Log("Unscaled size: " + unscaledSize);
         RecalculateUnscaledSize();
+        FixComponentPositions();
     }
 
     void RecalculateUnscaledSize()
     {
-        Vector3 leftEdgeSize = left_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
-        Vector3 rightEdgeSize = left_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
-        Vector3 topEdgeSize = left_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
-        Vector3 bottomEdgeSize = left_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
-
+        leftEdgeSize = left_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+        rightEdgeSize = right_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+        topEdgeSize = top_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+        bottomEdgeSize = bottom_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+        
         unscaledSize = new Vector3(0,0,0);
         unscaledSize[0] += leftEdgeSize[0] + topEdgeSize[0] + rightEdgeSize[0];
         unscaledSize[0] /= scaleTarget.localScale[0];
@@ -74,16 +100,8 @@ public class NewScaleAwayOnHit : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (!anvilAttachable.isOnAnvil) 
-        {
-            // Debug.Log("Weapon is not on anvil. Cannot modify weapon proportions.");
-            return;
-        }
-        if (collision.contactCount == 0 || !collision.gameObject.CompareTag("hammer")) 
-        {
-            // Debug.Log("No contact detected or collision is not with hammer");
-            return;
-        }
+        if (!anvilAttachable.isOnAnvil) { return; }
+        if (collision.contactCount == 0 || !collision.gameObject.CompareTag("hammer")) { return; }
         
         ContactPoint contact = collision.GetContact(0);
         Vector3 worldNormal = contact.normal;
@@ -91,18 +109,9 @@ public class NewScaleAwayOnHit : MonoBehaviour
 
         // VELOCITY
         float velocityMagnitude;
-        // Rigidbody hammerRb = collision.rigidbody; // the hammer
-        // float velocityMagnitude = hammerRb.linearVelocity.magnitude;
-        // float velocityMagnitude = collision.impulse.magnitude;
         Rigidbody hammerRb = collision.rigidbody;
         if (hammerRb == null) return;
         velocityMagnitude = hammerRb.linearVelocity.magnitude;
-        // Vector3 normal = contact.normal;
-        // velocityMagnitude = Mathf.Max(
-        //     0f,
-        //     Vector3.Dot(hammerRb.linearVelocity, -normal)
-        // );
-        // velocityMagnitude = collision.relativeVelocity.magnitude;
         if (velocityMagnitude < minVelocity || velocityMagnitude > maxVelocity) return;
         Debug.Log("Collision's velocity magnitude = " + velocityMagnitude);
         if (Time.time - lastHitTime < hitCooldown) return;
@@ -111,21 +120,74 @@ public class NewScaleAwayOnHit : MonoBehaviour
         float volumeShiftedOnHit = Mathf.Clamp01(velocityMagnitude / maxVelocity) * maxVolumeShift * temperatureScript.GetPercentMaxTemperature();
 
         HandleDirectionalScale(worldNormal, volumeShiftedOnHit);
-
-        // pendingNormal = normal;
-        // pendingVolumeShift = volumeShiftedOnHit;
-        // pendingScale = true;
     }
 
     void RecalculateMeasurements()
     {
-        scaledSize = new(unscaledSize[0] * scaleTarget.localScale[0], unscaledSize[1] * scaleTarget.localScale[1], unscaledSize[2] * scaleTarget.localScale[2]);
+        // scaledSize = new(unscaledSize[0] * scaleTarget.localScale[0], unscaledSize[1] * scaleTarget.localScale[1], unscaledSize[2] * scaleTarget.localScale[2]);
+
+        leftEdgeSize = left_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+        rightEdgeSize = right_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+        topEdgeSize = top_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+        bottomEdgeSize = bottom_edge.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+
+        // For box colliders, x = y, y = z, z = x
+
+        scaledSize = new(
+            leftEdgeSize[0] + topEdgeSize[0] + rightEdgeSize[0],
+            leftEdgeSize[1],
+            topEdgeSize[2] + leftEdgeSize[2] + bottomEdgeSize[2]
+        );
         volume = scaledSize[0] * scaledSize[1] * scaledSize[2];
         area = new(scaledSize[1] * scaledSize[2], scaledSize[0] * scaledSize[2], scaledSize[0] * scaledSize[1]);
 
         // Debug.Log("\nunscaledSize[0] * scaleTarget.localScale[0]: " + unscaledSize[0] * scaleTarget.localScale[0] + 
         //         "\nunscaledSize[1] * scaleTarget.localScale[1]: " + unscaledSize[1] * scaleTarget.localScale[1] + 
         //         "\nunscaledSize[2] * scaleTarget.localScale[2]: " + unscaledSize[2] * scaleTarget.localScale[2]);
+    }
+
+    void FixComponentPositions()
+    {
+        top_left_corner.transform.localPosition = new Vector3(
+            top_edge.transform.localPosition[0] - topEdgeSize[1] / 2, 
+            top_edge.transform.localPosition[1], 
+            top_edge.transform.localPosition[2]
+        );
+        Debug.Log("top_edge.transform.localPosition: " + top_edge.transform.localPosition);
+        Debug.Log("topEdgeSize: " + topEdgeSize);
+        Debug.Log("top_left_corner.transform.localPosition: " + top_left_corner.transform.localPosition);
+        Debug.Log("top_edge.transform.localPosition[0]: " + top_edge.transform.localPosition[0] + "\n - topEdgeSize[0] / 2:" + topEdgeSize[0] / 2 + "\n = top_left_corner.transform.localPosition: " + top_left_corner.transform.localPosition);
+        left_edge.transform.localPosition = new Vector3(
+            top_left_corner.transform.localPosition[0],
+            top_left_corner.transform.localPosition[1],
+            top_left_corner.transform.localPosition[2] + leftEdgeSize[1] / 2
+        );
+        bottom_left_corner.transform.localPosition = new Vector3(
+            left_edge.transform.localPosition[0], 
+            left_edge.transform.localPosition[1], 
+            left_edge.transform.localPosition[2] - leftEdgeSize[1] / 2
+        );
+        bottom_left_corner.transform.localPosition = new Vector3(
+            bottom_left_corner.transform.localPosition[0], 
+            bottom_left_corner.transform.localPosition[1], 
+            bottom_left_corner.transform.localPosition[2] + topEdgeSize[0] / 2
+        );
+
+        top_right_corner.transform.localPosition = new Vector3(
+            top_edge.transform.localPosition[0] + topEdgeSize[1] / 2, 
+            top_edge.transform.localPosition[1], 
+            top_edge.transform.localPosition[2]
+        );
+        right_edge.transform.localPosition = new Vector3(
+            top_right_corner.transform.localPosition[0],
+            top_right_corner.transform.localPosition[1],
+            top_right_corner.transform.localPosition[2] + leftEdgeSize[1] / 2
+        );
+        bottom_right_corner.transform.localPosition = new Vector3(
+            right_edge.transform.localPosition[0], 
+            right_edge.transform.localPosition[1], 
+            right_edge.transform.localPosition[2] + leftEdgeSize[1] / 2
+        );
     }
 
     void HandleDirectionalScale(Vector3 worldNormal, float volumeShiftedOnHit)
@@ -138,11 +200,16 @@ public class NewScaleAwayOnHit : MonoBehaviour
             Mathf.Abs(localNormal.z)
         );
 
-        Vector3 oldScale = scaleTarget.localScale;
+        // Vector3 oldScale = scaleTarget.localScale;
+        Vector3 oldScale = new(
+            (left_edge.transform.localScale[0] + top_edge.transform.localScale[0] + right_edge.transform.localScale[0]) / 3,
+            left_edge.transform.localScale[1],
+            (top_edge.transform.localScale[2] + left_edge.transform.localScale[2] + bottom_edge.transform.localScale[2]) / 3
+        );
         Vector3 newScale = oldScale;
         // Debug.Log("New Scale: " + newScale);
 
-        // Debug.Log("Volume After: " + volume);
+        Debug.Log("Volume After: " + volume);
         RecalculateMeasurements();
         float hitAxisSizeLost;
         float hitAxisShrinkFactor;
@@ -200,13 +267,31 @@ public class NewScaleAwayOnHit : MonoBehaviour
             newScale.y >= minScale.y && newScale.y <= maxScale.y &&
             newScale.z >= minScale.z && newScale.z <= maxScale.z)
         {
-            scaleTarget.localScale = newScale;
+            // scaleTarget.localScale = newScale;
+            foreach (GameObject component in allComponents)
+            {
+                Vector3 newComponentScale = component.transform.localScale;
+                newComponentScale = new(newComponentScale[0] * newScale.x, newComponentScale[1] * newScale.y, newComponentScale[2] * newScale.z);
+                component.transform.localScale = newComponentScale;
+            }
+
             onScaleChanged.Invoke(oldScale, newScale);
+            FixComponentPositions();
             return;
         }
         Debug.Log("Scale change rejected: newScale out of bounds");
     }
+
+    void MultiplyScaleAxis(Transform transform, float mult, char axis)
+    {
+        Vector3 newScale     = transform.localScale;
+        if (axis == 'x')       newScale.x *= mult; 
+        else if (axis == 'y')  newScale.y *= mult; 
+        else                   newScale.z *= mult;
+        transform.localScale = newScale;
+    }
 }
+
 
 /*
 using System.Collections.Generic;
