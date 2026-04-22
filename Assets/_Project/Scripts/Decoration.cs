f5b9378a414cf822b98889d15fbe97600c874fe7
ZhongTheKong <guoyizhong2004@gmail.com>
2026-04-22 19:41:00 -0400

    Added sword guards and have started work on sword decorations

---
+++ Assets/_Project/Scripts/Decoration.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class Decoration : MonoBehaviour
+{
+    Rigidbody rb;
+    Collider decoCollider;
+    protected Transform swordTransform;
+    protected Vector3 attachLocalPosition;
+    protected Quaternion attachLocalRotation = Quaternion.identity;
+    SwordDecorationHandler swordDecorationHandler;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        decoCollider = GetComponent<Collider>();
+    }
+    public void SetSwordToAttachTo(Transform newSwordTransform)
+    {
+        swordTransform = newSwordTransform;
+    }
+    public void SetLocalAttachPosition(Vector3 newAttachLocalPosition)
+    {
+        attachLocalPosition = newAttachLocalPosition;
+    }
+    public void SetLocalAttachRotation(Quaternion newAttachLocalRotation)
+    {
+        attachLocalRotation = newAttachLocalRotation;
+    }
+    public void SetSwordDecorationHandler(SwordDecorationHandler newSwordDecorationHandler)
+    {
+        swordDecorationHandler = newSwordDecorationHandler;
+
+        // AttachDecoration();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.contactCount == 0 || !collision.gameObject.CompareTag("hammer")) { return; }
+        if (swordTransform != null)
+        {
+            AttachDecoration();
+        }
+    }
+
+    void AttachDecoration()
+    {
+        gameObject.transform.SetParent(swordTransform);
+        decoCollider.enabled = false;
+        rb.isKinematic = true;
+        rb.useGravity = false;
+        // rb.enabled = false;
+        gameObject.transform.SetLocalPositionAndRotation(attachLocalPosition, attachLocalRotation);
+        swordDecorationHandler.OnDecorationAttach(gameObject.tag);
+    }
+}
---
+++ Assets/_Project/Scripts/SwordDecorationHandler.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordDecorationHandler : MonoBehaviour
+{
+    string targetName = "Sword Handle";
+    [SerializeField] GameObject sword;
+    [SerializeField] GameObject handle;
+    // readonly string targetName = "Sword Handle";
+    bool isGuardAttached = false;
+    bool isHandleAttached = false;
+    bool isPommelAttached = false;
+    readonly int handleBackForthScaleAxisIndex = 1;
+    List<Decoration> contactingDecorations = new();
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Debug.Log("Collision detected");
+        if (collision.contactCount == 0 || !collision.gameObject.TryGetComponent(out Decoration decoration)) { return; }
+        // Debug.Log("Collision contains decoration");
+        ContactPoint contact = collision.GetContact(0);
+        // Debug.Log("Collision name: " + contact.thisCollider.name);
+        if (contact.thisCollider.name != targetName) { return; }
+        // Debug.Log("Collision was with handle");
+
+        // Debug.Log("Name: " + collision.gameObject.name);
+        // Debug.Log("Tag: " + collision.gameObject.tag);
+        // foreach (var contact in collision.contacts)
+        // {
+        //     Debug.Log("Other collider tag: " + contact.otherCollider.tag);
+        // }
+        if (collision.gameObject.CompareTag("guard"))
+        {
+            if (isGuardAttached) return;
+            Debug.Log("Guard is touching handle");
+            contactingDecorations.Add(decoration);
+            decoration.SetSwordToAttachTo(sword.transform);
+            decoration.SetLocalAttachPosition(new(0,0,0));
+            decoration.SetLocalAttachRotation(Quaternion.Euler(-90f, 0f, 0f));
+            decoration.SetSwordDecorationHandler(this);
+        }
+        else if (collision.gameObject.CompareTag("handle"))
+        {
+            if (isHandleAttached) return;
+            Debug.Log("Handle is touching handle");
+            contactingDecorations.Add(decoration);
+            decoration.SetSwordToAttachTo(sword.transform);
+            decoration.SetLocalAttachPosition(new(0,-0.02f,0));
+            decoration.SetSwordDecorationHandler(this);
+        }
+        else if (collision.gameObject.CompareTag("pommel"))
+        {
+            if (isPommelAttached) return;
+            Debug.Log("Pommel is touching handle");
+            contactingDecorations.Add(decoration);
+            decoration.SetSwordToAttachTo(sword.transform);
+            decoration.SetLocalAttachPosition(new(0,-(0.2f * handle.transform.localScale[handleBackForthScaleAxisIndex]),0));
+            decoration.SetSwordDecorationHandler(this);
+        }
+
+        // if (
+        //     ((decoration is SwordGuard) && !isGuardAttached) ||
+        //     ((decoration is SwordGuard) && !isHandleAttached) ||
+        //     ((decoration is SwordGuard) && !isPommelAttached)
+        // )
+        // {
+        //     decoration.SetSwordToAttachTo(gameObject.transform);
+        // }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Decoration decoration = collision.gameObject.GetComponent<Decoration>();
+        if (!contactingDecorations.Contains(decoration)) { return; }
+        Debug.Log("Decoration is leaving handle");
+        contactingDecorations.Remove(decoration);
+        decoration.SetSwordToAttachTo(null);
+    }
+
+    public void OnDecorationAttach(string tag)
+    {
+        if (tag == "guard") isGuardAttached = true;
+        if (tag == "handle") isHandleAttached = true;
+        if (tag == "pommel") isPommelAttached = true;
+    }
+}
